7463683ebe940c68a7884dd59e6b3f8059cd97e4
Erik Baklund <erik@hubii.com>
2020-01-30 21:38:14 +0100

    Working custom server

--- src/server/dummy.ts
+++
@@ -1,8 +0,0 @@
-const _koko = new WeakMap<object, number>();
-class DummyClass {
-  constructor () {
-    _koko.set(this, 0);
-  }
-}
-
-export = DummyClass;
---
+++ src/server/server.ts
@@ -0,0 +1,18 @@
+'use strict'
+
+import express = require('express');
+import { AddressInfo } from 'net';
+import path = require('path');
+
+const app = express();
+
+const wwwroot = path.resolve('wwwroot');
+console.log('wwwroot:', wwwroot);
+
+app.use('/', express.static(wwwroot));
+app.use('/js', express.static(wwwroot + '/js'));
+
+const server = app.listen(8081, () => {
+    const port = (server.address() as AddressInfo).port;
+    console.log("Server started at http://localhost:%s", port);
+});
